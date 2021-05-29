e3d4ca9a5b936ac0a292add456a5d210184dd5eb
Michael Evans <evansrichardmichael@gmail.com>
2021-05-29 17:02:01 -0600

    add manifest, docker, and started service worker

---
+++ public/js/idb.js
@@ -0,0 +1,11 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_data', { autoIncrement: true });
+  };
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
