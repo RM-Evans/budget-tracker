0a87e101789fd1dc00efdc4b011a945bab3ed5be
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-05 17:27:20 -0600

    prepare to deploy to heroku

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,11 +1,133 @@
+
+
+
+// // "use strict";
+
+// // const pendingObjectStoreName = `pending`;
+
+// let db;
+
+// // create a new db request for a "budget" database.
+// const request = indexedDB.open(`budget_data`, 1);
+
+// request.onupgradeneeded = function (event) {
+//     const db = event.target.result;
+//     db.createObjectStore('new_transaction', { autoIncrement: true });
+// };
+
+// request.onsuccess = function (event) {
+//     if (navigator.onLine) {
+//         uploadTransaction();
+//     }
+// };
+
+// request.onerror = event => console.error(event);
+
+// function saveRecord(record) {
+//     const transaction = db.transaction(['new_transaction'], 'readwrite');
+//     const transactionObjectStore = transaction.objectStore('new_transaction');
+//     transactionObjectStore.add(record);
+// };
+
+
+// function uploadTransaction() {
+//     const transaction = db.transaction(['new_transaction'], 'readwrite');
+//     const transactionObjectStore = transaction.objectStore('new_transaction');
+//     const getAll = transactionObjectStore.getAll();
+
+//     getAll.onsuccess = function () {
+//         if (getAll.result.length > 0) {
+//             fetch('/api/transaction/bulk', {
+//                 method: 'POST',
+//                 body: JSON.stringify(getAll.result),
+//                 headers: {
+//                     'Accept': 'application/json, text/plain, */*',
+//                     'Content-Type': 'application/json'
+//                 }
+//             })
+//                 .then(response => response.json())
+//                 .then(serverResponse => {
+//                     if (serverResponse.message) {
+//                         throw new Error(serverResponse);
+//                     }
+
+//                     const transaction = db.transaction(['new_transaction'], 'readwrite');
+//                     const transactionObjectStore = transaction.objectStore('new_transaction');
+//                     transactionObjectStore.clear();
+
+//                     alert("New transactions submitted.");
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                 });
+//         }
+//     };
+// };
+
+// window.addEventListener('online', uploadTransaction);
+
+
+
+
+
 let db;
+const request = indexedDB.open('budget', 1);
 
-const request = indexedDB.open('budget_tracker', 1);
-
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-    // save a reference to the database 
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_data', { autoIncrement: true });
-  };
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+};
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    transactionObjectStore.clear();
+
+                    alert("You are back online. New transactions submitted.");
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+};
+
+window.addEventListener('online', uploadTransaction);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,37 @@
-'use strict'
+
+
+
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
 
 
 const FILES_TO_CACHE = [
     `/js/idb.js`,
     `/index.html`,
     `/js/index.js`,
-    `/css/index.css`,
+    `/css/styles.css`,
     `/manifest.webmanifest`,
+    `/icons/icon-72x72.png`,
+    `/icons/icon-96x96.png`,
+    `/icons/icon-128x128.png`,
+    `/icons/icon-144x144.png`,
+    `/icons/icon-152x152.png`,
     `/icons/icon-192x192.png`,
-    `/icons/icon-192x192.png`
+    `/icons/icon-384x384.png`,
+    `/icons/icon-512x512.png`,
 ];
 
-const STATIC_CACHE = `static-cache-v1`;
-const RUNTIME_CACHE = `runtime-cache`;
 
-self.addEventListener(`install`, event => {
-    event.waitUntil(
-        caches
-            .open(STATIC_CACHE)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
+// Install the service worker
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(STATIC_CACHE).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
+
+    self.skipWaiting();
 });
 
 self.addEventListener(`activate`, event => {
@@ -40,42 +51,21 @@ self.addEventListener(`activate`, event => {
     );
 });
 
-self.addEventListener(`fetch`, event => {
-    if (
-        event.request.method !== `GET` ||
-        !event.request.url.startsWith(self.location.origin)
-    ) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-
-    if (event.request.url.includes(`/api/transaction`)) {
-        event.respondWith(
-            caches.open(RUNTIME_CACHE).then(cache =>
-                fetch(event.request)
-                    .then(response => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                    .catch(() => caches.match(event.request))
-            )
-        );
-        return;
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            if (cachedResponse) {
-                return cachedResponse;
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request;
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request);
             }
 
-            return caches
-                .open(RUNTIME_CACHE)
-                .then(cache =>
-                    fetch(event.request).then(response =>
-                        cache.put(event.request, response.clone()).then(() => response)
-                    )
-                );
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
         })
     );
 });
